90154b897ffc3fadc2338ddcfa83b2a56f5f2dbf
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2018-11-16 14:00:32 -0600

    adding vue

---
+++ app.js
@@ -0,0 +1,8 @@
+var constuctMsg = new Vue({
+    el: '#main-container',
+    data: {
+        msg1: 'Hi there! Thanks for stopping by!',
+        msg2: 'This site is currently under construction. Please stop by soon for something great!',
+        msg3: '- Justin'
+    }
+});
