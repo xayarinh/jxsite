9e79ec8f0d2a81e13e09195f02bc3a32c3fedfd8
Justin Xayarinh <justinxayarinh@dhcp-10-159-240-191.vpn.oracle.com>
2019-02-21 13:26:58 -0600

    test commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var constuctMsg = new Vue({
-    el: '#main-container',
+    el: '#construct-msg',
     data: {
         msg1: 'Hi there! Thanks for stopping by!',
         msg2: 'This site is currently under construction. Please stop by soon for something great!',
