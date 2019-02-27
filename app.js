ebfb3c3d216d2e0bf2b5d7743880e18a2dfcd3be
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2019-02-27 17:05:56 -0600

    testing vue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var constuctMsg = new Vue({
     el: '#construct-msg',
     data: {
-        msg1: 'Hi there! Thanks for stopping by!',
+        msg1: 'Hi there!!! Thanks for stopping by!',
         msg2: 'This site is currently under construction. Please stop by soon for something great!',
         msg3: '- Justin'
     }
